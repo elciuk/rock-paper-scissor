9894f5a1b812bad3a97e4f14606a69cfd3065b8e
Pavel Cink <pavel.cink5@gmail.com>
2020-07-09 12:22:04 +0100

    add game logic and pop-up

--- script.js
+++ script.js
@@ -16,31 +16,43 @@ yourChoices.forEach(btnClick);
 function btnClick(item){
     item.onclick = function(){
         choice = item.textContent;
-        return makePlay(choice, computerPlay());
+        makePlay(choice, computerPlay());   
+        scoreEval();
         }
      }
 
+     function scoreEval(){
+        if(parseInt(yourScore.textContent) + parseInt(compScore.textContent) >= 5){
+            if (parseInt(yourScore.textContent) > parseInt(compScore.textContent)){
+                window.alert(`You win ${yourScore.textContent} to ${compScore.textContent}`);
+            } else {
+                window.alert(`You lose ${yourScore.textContent} to ${compScore.textContent}`);
+            }
+            yourScore.textContent = 0;
+            compScore.textContent = 0;
+        }
+    }
 
-
-
+function computerPlay(){
+    let selection = ["Rock", "Paper", "Scissors"];
+    return selection[Math.floor(Math.random()*selection.length)];
+ }
+    
+    
 function makePlay(myChoice, compChoice){
     if(myChoice == compChoice){
         return console.log(`draw ${myChoice} ${compChoice}` );
     } else if (myChoice == "Rock" && compChoice == "Scissors" ||
                 myChoice == "Paper" && compChoice =="Rock" ||
-                myChoice == "Scissors" && compChoice == "Paper"){
+                myChoice == "Scissors" && compChoice == "Paper")
+                {
+                    yourScore.textContent = parseInt(yourScore.textContent) + 1;
                     return console.log(`win ${myChoice} ${compChoice}`);
                 } else {
+                    compScore.textContent = parseInt(compScore.textContent) +1;
                     return console.log(`lose ${myChoice} ${compChoice}`);
                 }
     
             }    
-
-
-function computerPlay(){
-    let selection = ["Rock", "Paper", "Scissors"];
-
-    return selection[Math.floor(Math.random()*selection.length)];
-}
 
 

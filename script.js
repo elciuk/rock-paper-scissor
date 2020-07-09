04a00c218c98ef1abd6610ad51105e8458487977
Pavel Cink <pavel.cink5@gmail.com>
2020-07-09 11:01:59 +0100

    initial commit, add structure and styling

---
+++ script.js
@@ -0,0 +1,7 @@
+function computerPlay(){
+    let selection = ["rock", "paper", "scissors"];
+    return selection[Math.floor(Math.random()*selection.length)];
+}
+function decision(playerSelection, computerSelection){
+    
+}
